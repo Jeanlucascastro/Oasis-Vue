38585c7c73656d7db3227fa0b8c90dcb0d38c32a
Jean Castro <61846534+Jeanlucascastro@users.noreply.github.com>
2023-11-15 22:11:14 -0300

    Converting nuxt to Vue

---
+++ src/views/AboutView.vue
@@ -0,0 +1,61 @@
+'use strict'
+<template>
+  <div class="about">
+    <h1>This is an about page {{ loop }}</h1>
+    <YoutubeVue3 ref="youtube" :videoid="video_id" :loop="loop" :width="480" :height="320"
+     @ended="onEnded" @paused="onPaused" @played="onPlayed"/>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+<script lang="ts">
+import { YoutubeVue3 } from 'youtube-vue3'
+import { useRoute } from 'vue-router';
+import { ref } from 'vue';
+
+export default {
+
+ name: 'App',
+ data() {
+   return {
+     video_id: "kwpGBT-cQ-M", 
+   }
+ },
+ setup() {
+  const route = useRoute();
+  const loop = ref(1);
+  
+  if (Array.isArray(route.params.id)) {
+    // handle array case
+  } else {
+    loop.value = Number(route.params.id);
+  }
+  
+  return { loop };
+ },
+ components: {
+   YoutubeVue3,
+ },
+ methods: {
+   onEnded() {
+     console.log("## OnEnded")
+   },
+   onPaused() {
+     console.log("## OnPaused")
+   },
+   onPlayed() {
+     console.log("## OnPlayed")
+   },
+ },
+}
+
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import axios from 'axios'
+import { ref, onMounted } from 'vue'
+
+const user = ref('')
+const token = ref('')
+const password = ref('')
+const email = ref('')
+
+const fetchData = async () => {
+  try {
+    const response = await axios
+      .post(
+        'http://192.168.0.104:3001/api/auth/login',
+        {
+          email: "jean@gmail.com",
+          password: "^X9$jP0#aBHxD5"
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+          }
+        }
+      )
+      .then((usuario) => {
+        user.value = usuario.data.usuario
+        token.value = usuario.data.token
+        password.value = usuario.data.password
+        email.value = usuario.data.email
+        sessionStorage.setItem('token-oasis', usuario.data.token)
+        sessionStorage.setItem('usuario-oasis', usuario.data.usuarioId)
+        sessionStorage.setItem('company-oasis', usuario.data.companyId)
+
+        console.log('company-oasis', usuario)
+      })
+  } catch (error) {
+    console.error('Error fetching data:', error)
+  }
+}
+
+onMounted(() => {
+  fetchData()
+})
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
